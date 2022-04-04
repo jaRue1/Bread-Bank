6228cf271c6e25b8b9e2146357b59c0cd071dfba
jaRue1 <jaruejohnson@outlook.com>
2022-04-04 02:17:27 -0400

    added chavas requests

--- index.js
+++ index.js
@@ -92,10 +92,6 @@ console.log(users)
 
 // .sort() this array alphabetically
 
-// SALES DEPT - This is
-
-// get the top 5 earners in company Tech A and Tech B
-
 // shift
 
 // unshift()
@@ -103,3 +99,13 @@ console.log(users)
 // pop()
 
 // reduce()
+
+// SALES DEPT - Chava from sales needs to know somethings before a meeting see if you can help him out.
+
+// Hey bud , could you get me the top 5 earners in company Tech A and company Tech B
+// ps - I just need their last name, job title, address, and phone number
+
+// Hey bud , could you get me the average balance of users in company Tech C
+
+// Hey bud, could you get me all of the users in companies Tech A and Tech D that have a balance over 50,000
+// ps - I just need their last name, email and address
