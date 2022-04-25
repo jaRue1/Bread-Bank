d2bff1f4f365c38512e4d26596eb62c5de03e1c4
jarue1 <jaruejohnson@outlook.com>
2022-04-25 12:33:04 -0400

    added unshift and shift examples

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const faker = require("faker")
-const { fa } = require("faker/lib/locales")
+const faker = require("faker");
+const { fa } = require("faker/lib/locales");
 
 // Lets make some fake users
 
 function makeFakeUsers() {
-  let fakeUsers = []
+  let fakeUsers = [];
   for (let i = 0; i < 100; i++) {
-    let firstName = faker.name.firstName()
-    let lastName = faker.name.lastName()
-    let jobTitle = faker.name.jobTitle()
-    let state = faker.address.state()
-    let city = faker.address.city()
-    let zipCode = faker.address.zipCode("#####")
-    let streetAddress = faker.address.streetAddress(true)
-    let country = faker.address.country()
-    let phoneNumber = faker.phone.phoneNumber()
-    let accountNumber = faker.finance.account(12) // give a random 12 digit account #
-    let balance = Number(faker.finance.amount(100, 1000000, 2))
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    let jobTitle = faker.name.jobTitle();
+    let state = faker.address.state();
+    let city = faker.address.city();
+    let zipCode = faker.address.zipCode("#####");
+    let streetAddress = faker.address.streetAddress(true);
+    let country = faker.address.country();
+    let phoneNumber = faker.phone.phoneNumber();
+    let accountNumber = faker.finance.account(12); // give a random 12 digit account #
+    let balance = Number(faker.finance.amount(100, 1000000, 2));
     if (i <= 20) {
-      const routingNumber = 111111111
-      const company = "Tech A"
+      const routingNumber = 111111111;
+      const company = "Tech A";
       fakeUsers.push({
         id: i,
         firstName: firstName,
@@ -35,10 +35,10 @@ function makeFakeUsers() {
         currentBalance: balance,
         routingNumber: routingNumber,
         company: company,
-      })
+      });
     } else if (i >= 21 && i <= 50) {
-      const routingNumber = 222222222
-      const company = "Tech B"
+      const routingNumber = 222222222;
+      const company = "Tech B";
       fakeUsers.push({
         id: i,
         firstName: firstName,
@@ -53,10 +53,10 @@ function makeFakeUsers() {
         currentBalance: balance,
         routingNumber: routingNumber,
         company: company,
-      })
+      });
     } else if (i >= 51 && i <= 70) {
-      const routingNumber = 333333333
-      const company = "Tech C"
+      const routingNumber = 333333333;
+      const company = "Tech C";
       fakeUsers.push({
         id: i,
         firstName: firstName,
@@ -72,10 +72,10 @@ function makeFakeUsers() {
         currentBalance: balance,
         routingNumber: routingNumber,
         company: company,
-      })
+      });
     } else if (i >= 71 && i <= 99) {
-      const routingNumber = 444444444
-      const company = "Tech D"
+      const routingNumber = 444444444;
+      const company = "Tech D";
       fakeUsers.push({
         id: i,
         firstName: firstName,
@@ -90,16 +90,16 @@ function makeFakeUsers() {
         currentBalance: balance,
         routingNumber: routingNumber,
         company: company,
-      })
+      });
     }
   }
-  return fakeUsers
+  return fakeUsers;
 }
 
-let users = makeFakeUsers() // this becomes array of 100 objects
+let users = makeFakeUsers(); // this becomes array of 100 objects
 
-console.log("All Users !!")
-console.log(users)
+console.log("All Users !!");
+console.log(users);
 
 // BASICS - The following tasks have been given below to help you get
 // familiar with JS built in array methods. Try to solve these problems with the
@@ -107,48 +107,72 @@ console.log(users)
 
 // .find() a user that has an id that matches the your birthday date i.e (21)
 let findFakeUser = users.find((user) => {
-  return user.id === 21
-})
-console.log("User # 21 !")
-console.log(findFakeUser)
+  return user.id === 21;
+});
+console.log("User # 21 !");
+console.log(findFakeUser);
 
 /// .map() all of the users and output just their firstNames
 let fakeUsersFirstNames = users.map((user) => {
-  return user.firstName
-})
-console.log("All User First Names !!")
-console.log(fakeUsersFirstNames)
+  return user.firstName;
+});
+console.log("All User First Names !!");
+console.log(fakeUsersFirstNames);
 
 // .filter() out all users with an id greater than 30 but less 45 (31 to 44)
 let chosenFakeUsers = users.filter((user) => {
   if (user.id > 30 && user.id < 45) {
-    return user
+    return user;
   }
-})
-console.log("The Chosen 15")
-console.log(chosenFakeUsers)
+});
+console.log("The Chosen 15");
+console.log(chosenFakeUsers);
 
 // .forEach() user in company TECH B print out there names in Alphabetical Order
-const companyUserNames = []
+const companyUserNames = [];
 function getNames() {
   users.forEach((user) => {
     if (user.company == "Tech B")
       // push()
-      companyUserNames.push(user.firstName + " " + user.lastName)
-  })
+      companyUserNames.push(user.firstName + " " + user.lastName);
+  });
 }
-getNames()
-console.log("Tech B Names !!")
+getNames();
+console.log("Tech B Names !!");
 
 // .sort() this array alphabetically
-companyUserNames.sort()
+companyUserNames.sort();
 for (const iterator of companyUserNames) {
-  console.log(iterator) // print out each name
+  console.log(iterator); // print out each name
 }
 
 // shift
+const beforeShift = users.length;
+const removedUser = users.shift();
+const afterShift = users.length;
+
+console.log(" Removed User at index 0" + removedUser);
+console.log("length before " + beforeShift + "length after " + afterShift);
 
 // unshift()
+const beforeUnShift = users.length;
+const addedUser = users.unshift({
+  firstName: "Added",
+  lastName: "User",
+  jobTitle: "jobTitle",
+  city: "city",
+  zipCode: 111111,
+  streetAddress: "streetAddress",
+  country: "country",
+  phoneNumber: 123456789,
+  accountNumber: 122947834,
+  currentBalance: 10000,
+  routingNumber: "routingNumber",
+  company: "company",
+});
+const afterUnShift = users.length;
+console.log(" Added a User at index " + addedUser);
+console.log("length before " + beforeUnShift + " length after " + afterUnShift);
 
 // pop()
 
@@ -162,17 +186,17 @@ function getTopEarnersInCompany(users, company) {
   return [...users]
     .filter((user) => user.company === company)
     .sort((a, b) => {
-      if (a.currentBalance < b.currentBalance) return -1
-      if (a.currentBalance > b.currentBalance) return 1
-      return 0
+      if (a.currentBalance < b.currentBalance) return -1;
+      if (a.currentBalance > b.currentBalance) return 1;
+      return 0;
     })
-    .slice(-5) // slice()
+    .slice(-5); // slice()
 }
 // find top earners within users array
-const company1 = getTopEarnersInCompany(users, "Tech A")
-const company2 = getTopEarnersInCompany(users, "Tech B")
-console.log("Company A Top Earners", company1)
-console.log("Company B Top Earners", company2)
+const company1 = getTopEarnersInCompany(users, "Tech A");
+const company2 = getTopEarnersInCompany(users, "Tech B");
+console.log("Company A Top Earners", company1);
+console.log("Company B Top Earners", company2);
 
 // Hey bud , could you get me the average balance of users in company Tech C
 
